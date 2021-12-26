3a0a535d7fead6dd79217129cafea6cdb7e38eea
Gavin Bender <gavinben@icloud.com>
2021-12-26 16:32:09 -0500

    Project Completed and Bug tested Goal = "To meet expectations"

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-TechDegree-Project 1
- 
+The goal is produce a simple numbers game for a random number between a range of 1 and 15
+Requirement is to "Meet Project Standards" per Treehouse guidelines
---
+++ guessing_game.py
@@ -0,0 +1,54 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+Created via VS Code IDE
+
+"""
+
+import random
+
+
+def start_game():
+    
+
+
+    name = input("Hello, welcome to the number guessing game. Please enter your name ")
+    num = random.randint(1,15)
+    attempts = 0
+
+    while True:    
+        try:
+            guess = int(input("{}, please select a random number between 1 through 15. ".format(name)))
+           
+            attempts += 1
+            if guess < 1:
+                print("Sorry, this number is outside of the stated range 1 and 15")
+            elif guess > 10:
+                print("Sorry, this number is outside of the stated range range 1 and 15")
+            elif guess > num:
+                print("It's lower")
+            elif guess < num:
+                print("It's higher")
+            else:
+                if guess == num:
+                    print("Great job {} ! you have selected the correct answer in a total of {} attempts".format(name,attempts))
+                break
+        
+        except ValueError:
+            print("The following value is not a number. Please try again and select a number between 1 and 15")    
+            continue    
+            
+    newgame = input("Thanks for playing {}. WOuld you like to play again? Yes/No ".format(name))
+    if newgame.lower() == "yes":
+        start_game()
+    else:
+        print("Good Bye")
+
+
+    
+
+
+    
+start_game()
