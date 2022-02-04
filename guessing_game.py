39f3bec2444a5a20713d133ea5a973d2624b6ddd
Gavin Bender <gavinben@icloud.com>
2022-02-04 02:28:30 -0500

    Refactor to amend from Format to F

--- guessing_game.py
+++ guessing_game.py
@@ -17,7 +17,7 @@ def start_game():
 
     while True:
         try:
-            guess = int(input("{}, please select a random number between 1 through 15. ".format(name)))
+            guess = int(input(f"{name}, please select a random number between 1 through 15. "))
             attempts += 1
             if guess < 1:
                 print("Sorry, this number is outside of the stated range 1 and 15")
@@ -29,18 +29,18 @@ def start_game():
                 print("It's higher")
             else:
                 if guess == num:
-                    print("Great job {} ! you have selected the correct answer in a total of {} attempts".format(name, attempts))
+                    print(f"Great job {name}! you have selected the correct answer in a total of {attempts} attempts")
                 break
 
         except ValueError:
             print("The following value is not a number. Please try again and select a number between 1 and 15")
             continue
 
-    newgame = input("Thanks for playing {}. Would you like to play again? Yes/No ".format(name))
+    newgame = input(f"Thanks for playing {name}. Would you like to play again? Yes/No ")
     if newgame.lower() == "yes":
         start_game()
     else:
-        print("Good bye and have a great day!"))
+        print(f"Good bye {name} and have a great day!")
 
 
     
